3190977f47c816d7e38e8f8d85fd6d1fd5194668
HuannAnd <huannvicente14@outlook.com>
2025-02-18 11:46:25 -0300

    refactor: ajeitando algumas estilizações de Features.css e de Footer.css

--- src/pages/Homepage/components/Features/Features.jsx
+++ src/pages/Homepage/components/Features/Features.jsx
@@ -3,32 +3,33 @@ import './Features.css'
 
 export default function Features() {
   return (
-    <section>
-      <div className="title__features">
-        <h3 className="text-blue">Nossas funcionalidades </h3>
-        <h1 className="text-blue">O que te <span className="text-pink">Oferecemos</span></h1>
+    <section className="features">
+      <div className="features__title">
+        <h3 className="features__footnote">Nossas funcionalidades </h3>
+        <h1 className="text-blue">O que te <span className="text-pink">Oferecemos ?</span></h1>
       </div>
+      <p className="features__description">Observe abaixo algumas das nossas funcionalidades</p>
       <div className="features__cards">
-        <div className="card feature__1">
-        <UserRoundCog className="feature__icon" />
-        <p>Atualize seus dados do Cadunico com facilidade e eficácia sem mais perder tanto tempo, se modernize conosco</p>
-        <h4>Atualização cadastral</h4>
-        </div>
-        <div className="card feature__2">
-          <HousePlus className="feature__icon" />
-          <p>Agende de maneira rápida o serviço da visita domiciliar do CRAS , nos horários disponiveis</p>
-          <h4>Agendamento de visita domiciliar</h4>
-        </div>
-        <div className="card feature__3">
-          <Brain className="feature__icon" />
-          <p>Também consiga agendar o atendimento e apoio psicológico  com alguns dos profissionais da assistência social</p>
-          <h4>Atendimento psicologico</h4>
-        </div>
-        <div className="card feature__4">
-          <UserRoundPlus className="feature__icon" />
-          <p>Nosso sistema também te ajuda ao invés de ir ate a central do cras para fazer seu cadunico, fazer tudo do conforto da sua casa</p>
-          <h4>CADUNico</h4>
-        </div>
+        <article className="card">
+          <UserRoundCog className="features__icon" />
+          <p className="features__card__description">Atualize seus dados do Cadunico com facilidade e eficácia sem mais perder tanto tempo, se modernize conosco</p>
+          <h4 className="features__card__name">Atualização cadastral</h4>
+        </article>
+        <article className="card">
+          <HousePlus className="features__icon" />
+          <p className="features__card__description">Agende de maneira rápida o serviço da visita domiciliar do CRAS , nos horários disponiveis</p>
+          <h4 className="features__card__name">Agendamento de visita domiciliar</h4>
+        </article>
+        <article className="card">
+          <Brain className="features__icon" />
+          <p className="features__card__description">Também consiga agendar o atendimento e apoio psicológico  com alguns dos profissionais da assistência social</p>
+          <h4 className="features__card__name">Atendimento psicologico</h4>
+        </article>
+        <article className="card">
+          <UserRoundPlus className="features__icon" />
+          <p className="features__card__description">Nosso sistema também te ajuda ao invés de ir ate a central do cras para fazer seu cadunico, fazer tudo do conforto da sua casa</p>
+          <h4 className="features__card__name">CADUNico</h4>
+        </article>
       </div>
     </section>
   );
