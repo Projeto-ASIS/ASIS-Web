677ab0e279475c3be2fcfd85c83d72e37f4dfbf2
HuannAnd <huannvicente14@outlook.com>
2025-02-18 10:07:36 -0300

    feat: adicionando max-content-width em styleguide.css, adicionando Footer.jsx/css

--- src/common/components/Footer/Footer.jsx
+++ src/common/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 
-export default function Footer(){
+export default function Footer() {
   return (
-   <>
-   </>  
+    <footer className="footer">
+      <div className="footer__top">
+        <div className="footer__top__menu"></div>
+        <img src="" alt="" className="footer__top__logo" />
+        <ul className="footer__top__social-medias">
+        </ul>
+      </div>
+      <p className="footer__auth"></p>
+    </footer>
   )
 }
