36b02a54e03f0bbbf23de49c1d60a7dc6327d3d1
Gylleadhe ferreira <Gylleadheinacioferreira@gmail.com>
2025-02-20 10:46:15 -0300

    Criando About

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./pages/About/About"
 import Homepage from "./pages/Homepage/Homepage"
 
 export function App() {
   return (
     <>
-      <Homepage />
+      <About />
+
     </>
   )
 }
