484a4e32d956bc5a4d5c068cbc453ec2767cf51e
HuannAnd <huannvicente14@outlook.com>
2025-03-10 10:07:15 -0300

    feat: Adicionando Icon para o input

--- src/common/components/Input/Input.jsx
+++ src/common/components/Input/Input.jsx
@@ -8,10 +8,12 @@ Input.propTypes = {
   onChange: PropTypes.func,
   hasIcon: PropTypes.bool,
   placeholder: PropTypes.string,
-  required: PropTypes.bool
+  required: PropTypes.bool,
+  icon: PropTypes.node
+
 }
 
-export default function Input({ hasIcon = false, placeholder, onChange, required,  ...props }) {
+export default function Input({ hasIcon = false, icon: Icon, placeholder, onChange, required, ...props }) {
   const [isEmpty, setIsEmpty] = useState(true)
 
   const handleOnChange = useCallback((e) => {
@@ -29,8 +31,9 @@ export default function Input({ hasIcon = false, placeholder, onChange, required
       <label htmlFor="" className={`input__label input__label--${isEmpty ? "empty" : "not-empty"}`}>
         {placeholder}
         {required ? <span className="input__container__required-icon">*</span> : null}
-        </label>
+      </label>
       <input onChange={handleOnChange} className={`input__base ${props.className ?? ""} ${hasIcon ? "input__base--has-icon" : ""} input__base--${isEmpty ? "empty" : "not-empty"}`} type="text" {...props} />
+      {hasIcon && Icon ? <Icon className="input__icon" /> : null}
     </div>
   )
 }
