217308af74d7322f57ce521306f39ff5910eaa2a
GylleadheDev <Gylleadheinacioferreira@gmail.com>
2025-03-11 15:33:38 -0300

    imagem de auth

--- src/pages/Auth/AuthLayout.jsx
+++ src/pages/Auth/AuthLayout.jsx
@@ -1,17 +1,12 @@
 import './AuthLayout.css'
-
-import shape from '../../assets/shapeauth.png'
-import Logo from '../../assets/LogoASISCentralizada.png'
 import { Outlet } from "react-router-dom"
-import { ChevronLeft } from 'lucide-react'
+
 
 export default function AuthLayout({ children }) {
   return <>
     <main className='auth-container'>
 
-      <div className="auth-image">
-        
-      </div>
+      <div className="auth-image" >.</div>
       <>
       
       <Outlet />
