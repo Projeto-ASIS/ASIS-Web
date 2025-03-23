97affd6efa3decca756628ca5435be7aa0f72bf3
HuannAnd <huannvicente14@outlook.com>
2025-03-23 15:37:25 -0300

    refactor: trocando state para reducer

--- src/common/contexts/HighContrastProvider.jsx
+++ src/common/contexts/HighContrastProvider.jsx
@@ -1,16 +1,28 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useReducer, useEffect, useState, useContext } from "react"
+
 
 const HighContrastContext = createContext()
 const SetHighContrastContext = createContext()
 
-const EContrastValues = {
-  DARK: "dark",
-  LIGHT: "light"
+function reducer(state, action){
+  if(action.type === "toggle"){
+    return !state
+  }
+
+  if(action.type === "setter"){
+    return {
+      ...state,
+      ...action
+    }
+  }
+
+  throw Error("Input inválido do \"type\" para alto contraste")
 }
 
 export default function HighContrastProvider({ children }) {
   const initialColorPreference = localStorage.getItem("user-high-contrast")
-  const [contrastPreference, setContrastPreference] = useState(initialColorPreference)
+  // const [contrastPreference, setContrastPreference] = useState(initialColorPreference)
+  const [contrastPreference, colorDispatcher] = useReducer(reducer, )
 
   useEffect(() => {
     const isHighContrast = Boolean(document.body.dataset.highContrast)
@@ -18,15 +30,23 @@ export default function HighContrastProvider({ children }) {
     localStorage.setItem("user-high-contrast", isHighContrast)
 
     const body = document.body
-    body.dataset.highContrast = contrastPreference
+    body.dataset.highContrast = Boolean(contrastPreference)
 
   }, [contrastPreference])
 
   return (
     <HighContrastContext.Provider value={contrastPreference}>
-      <SetHighContrastContext.Provider value={setContrastPreference}>
+      <SetHighContrastContext.Provider value={colorDispatcher}>
         {children}
       </SetHighContrastContext.Provider>
     </HighContrastContext.Provider>
   )
+}
+
+export function useUserPreferences(){
+  return useContext(HighContrastContext)
+}
+
+export function usePreferencesDispatcher(){
+  return useContext(SetHighContrastContext)
 }
